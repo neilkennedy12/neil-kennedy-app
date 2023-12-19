43cc50ef04b73df67ff74705877be21efe8502d1
Neil Kennedy <nmk2142@columbia.edu>
2023-12-19 11:17:15 -0500

    some styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,53 @@
 import "./App.css";
 import { Home } from "./components/Home";
+import React from "react";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { teal } from "@mui/material/colors";
+import { AppBar, ThemeProvider, createTheme } from "@mui/material";
+import Logo from "./images/Logo.png";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+const theme = createTheme({
+  palette: {
+    primary: teal,
+    secondary: {
+      main: "#8bc34a",
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div>
+        <BrowserRouter>
+          <AppBar position="static" style={{ height: 80 }}>
+            <div
+              style={{
+                // justifyContent: "center",
+                paddingLeft: 10,
+                alignItems: "center",
+                display: "flex",
+              }}
+            >
+              <img src={Logo} alt="Logo" width={50} height={50} />
+              <h1 style={{ marginLeft: 30, fontWeight: "bold" }}>
+                Neil Kennedy
+              </h1>
+            </div>
+          </AppBar>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            {/* <Route path="/about" element={<About />} />
+  <Route path="/experience" element={<Experience />} /> */}
+            {/* ... add routes for all your desired pages ... */}
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </ThemeProvider>
   );
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,44 @@
+import React from "react";
+import styled from "@emotion/styled";
+import { Link } from "@mui/material";
+
 export const Home = () => {
-  <div>Neil Kennedy</div>;
+  const Header = styled.header`
+    background-color: #333;
+    color: #fff;
+    padding: 20px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  `;
+
+  const Nav = styled.nav`
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    gap: 10;
+  `;
+
+  const CustomLink = styled(Link)`
+    display: inline-block;
+    margin: 0 10px;
+    color: #fff;
+    text-decoration: none;
+  `;
+
+  // ... continue styling other components using styled components
+
+  return (
+    <div className="App">
+      <Header>
+        <Nav>
+          <CustomLink to="/">Welcome </CustomLink>
+          <CustomLink to="/about">About</CustomLink>
+          <CustomLink to="/projects">Projects</CustomLink>
+          <CustomLink to="/contact">Contact</CustomLink>
+        </Nav>
+      </Header>
+      {/* ... remaining components with styled components ... */}
+    </div>
+  );
 };
